e2b68f09a199f4e9da1c31d9579fb1fe73e4cd0f
Maxime J <125677181+maxxxiiime@users.noreply.github.com>
2023-05-27 16:09:43 +0200

    ajout du login.js test

---
+++ FrontEnd/login.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const errorMsg = document.getElementById('message-error');
+
+  try {
+    const response = await fetch('http://localhost:5678/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const token = data.token;
+
+      window.location.href = './index.html';
+    } else {
+      errorMsg.innerText = 'email ou mot de passe incorrect.';
+    }
+  } catch (error) {
+    console.log('Erreur lors de la requête de connexion :');
+  }
+});
